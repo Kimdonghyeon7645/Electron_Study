ea03b9266ef04cfa02d9946305bade8cc0dfad44
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2023-09-01 16:28:22 +0900

    chone : 콘솔 경고뜨는 부분 수정

--- electron-lab-app/src/assets/DownArrow.js
+++ electron-lab-app/src/assets/DownArrow.js
@@ -1,7 +1,7 @@
 const DownArrow = ({ fill = "#000000" }) => (
   <svg height="12" width="12" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
     <polygon
-      fill-rule="evenodd"
+      fillRule="evenodd"
       points="13.293 4.293 14.707 5.707 8 12.414 1.293 5.707 2.707 4.293 8 9.586"
     />
   </svg>
